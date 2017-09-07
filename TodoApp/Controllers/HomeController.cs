e5e996835f2920d47fa902ee28b93558898f7736
Sam <scharif01@gmail.com>
2017-09-07 12:38:25 -0400

    initial commit

---
+++ TodoApp/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodoList.Controllers
+{
+    public class HomeController : Controller
+    {
+        #region Public Methods
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #endregion
+    }
+}
